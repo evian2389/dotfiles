34fe4da97a8546961787ce39b42daa3e28cd5cbd
jongho3.lee <jongho3.lee@lge.com>
2017-01-17 16:16:03 +0900

    ver 0.71

---
+++ bin/dltlog.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh root@10.186.128.49 "dlt-receive -a localhost $1"
---
+++ bin/git_hooks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+for dir in * ; do
+	if [[ -d "$dir/.git" ]]; then
+		printf "\n===== $dir=====\n";
+		pushd .
+		cd "$dir";
+		scp -p -P 29411 jongho3.lee@mod.lge.com:hooks/commit-msg ./.git/hooks;
+		popd
+	fi
+done
+
+scp -p -P 29411 jongho3.lee@mod.lge.com:hooks/commit-msg ./.git/hooks
