67776d31bffc59cb87208bd036f56ffda71665e1
jongho3.lee <jongho3.lee@lge.com>
2021-08-24 10:07:43 +0900

    refresh2

---
+++ .config/.xmonad/startup.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+xrandr --output HDMI1 --mode 1920x1080 --primary --auto --output HDMI2 --mode 1920x1080 --right-of HDMI1 --auto
+
+# System tray
+if [ -z "$(pgrep trayer)" ] ; then
+    trayer --edge top \
+           --align right \
+           --widthtype percent \
+           --height 24 \
+           --alpha 0 \
+           --transparent true \
+           --width 5 \
+           --tint 0x282c34 &
+fi
+
+# Power manager
+if [ -z "$(pgrep xfce4-power-manager)" ] ; then
+    xfce4-power-manager &
+fi
+
+# Taffybar
+# if [ -z "$(pgrep taffybar)" ] ; then
+#     taffybar &
+# fi
+
+# Redshift
+if [ -z "$(pgrep redshift)" ] ; then
+    redshift &
+fi
+
+# Autolock
+# if [ -z "$(pgrep xautolock)" ] ; then
+    # xautolock -time 1 -locker "if ! grep 'RUNNING' /proc/asound/card*/pcm*/sub*/status;then xscreensaver-command -lock; else echo 'Sound on'; fi"
+# fi
+
+# Wallpaper
+if [ -z "$(pgrep nitrogen)" ] ; then
+    nitrogen --restore &
+fi
+
+# Screensaver
+if [ -z "$(pgrep xscreensaver)" ] ; then
+    xscreensaver -no-splash &
+fi
+
+# compton
+if [ -z "$(pgrep compton)" ] ; then
+    compton -b &
+fi
+
+# Network Applet
+if [ -z "$(pgrep nm-applet)" ] ; then
+    nm-applet &
+fi
+
+# Google Drive
+if [ -z "$(pgrep insync)" ] ; then
+    insync start &
+fi
+
+feh --bg-scale /home/jongho3/Pictures/Wallpapers/gr.jpg &
+barrier
+udiskie &
+
+# xbindkeys
+#xbindkeys
